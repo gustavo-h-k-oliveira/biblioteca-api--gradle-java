865652ec1e065b2ec2d0b1eff05bc855e6637673
Gustavo H. K. Oliveira <123880096+gustavo-h-k-oliveira@users.noreply.github.com>
2025-03-11 17:43:37 +0000

    Adiciona construtor para Livro a partir de LivroDTO e configura geração automática de ID.

--- src/main/java/application/model/Livro.java
+++ src/main/java/application/model/Livro.java
@@ -1,21 +1,31 @@
 package application.model;
 
+import application.record.LivroDTO;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 public class Livro {
     // Identificador único da tarefa
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
     private String titulo;
-
     private String generos;
-
     private String autores;
+
+    public Livro(LivroDTO record) {
+        this.id = record.id();
+        this.titulo = record.titulo();
+        this.generos = record.generos();
+        this.autores = record.autores();
+    }
 }
